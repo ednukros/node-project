95dd5d6af2a77424be79c8b99d89b3190e90730a
ednukros <isanmenarguez@gmail.com>
2023-01-20 10:12:40 +0100

    feat: index controllers & index routes

---
+++ src/api/index/index.controller.js
@@ -0,0 +1,17 @@
+const { model } = require("mongoose");
+
+const indexGet = (req, res, next) => {
+
+    return res.status(200).json('API ROOT');
+};
+
+const statusGet = (req, res, next) => {
+
+    return res.status(200).json('Status OK - Server Working');
+};
+
+module.exports = {
+
+    indexGet,
+    statusGet
+}
---
+++ src/api/index/index.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const controller = require('./index.controller');
+
+const router = express.Router();
+
+router.get('/', controller.indexGet);
+router.get('/status', controller.statusGet);
